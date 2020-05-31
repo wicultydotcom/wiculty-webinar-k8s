e6821407f2551d9cc6628b0e6fda96bd271f44ab
P Nageswara Rao <info@gamutgurus.com>
2020-05-31 22:04:21 +0530

    Project code

---
+++ src/main/java/com/gamutkart/App.java
@@ -0,0 +1,14 @@
+package com.gamutkart;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+		int i;
+// iterate for 800 times
+		for(i=0;i<=800;i++)
+		{
+			i += 4;
+   		}
+	 }
+}
